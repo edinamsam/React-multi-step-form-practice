97bbb0e5d0eb46607b60e7b31aa8ae974958dbf0
Ed <edinamsam4@gmail.com>
2026-01-11 13:03:08 +0000

    Persist form data using localstorage

--- src/Components/FormContext.jsx
+++ src/Components/FormContext.jsx
@@ -3,18 +3,27 @@ import { createContext, useContext, useState } from "react";
 const FormContext = createContext();
 
 export const FormProvider = ({ children }) => {
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    phone: "",
-    plan: "Arcade",
-    billing: "monthly",
-    addons: {
-      online: false,
-      storage: false,
-      profile: false,
-    },
+  const [formData, setFormData] = useState(() => {
+    const savedData = localStorage.getItem("formData");
+    return savedData
+      ? JSON.parse(savedData)
+      : {
+          name: "",
+          email: "",
+          phone: "",
+          plan: "Arcade",
+          billing: "monthly",
+          addons: {
+            online: false,
+            storage: false,
+            profile: false,
+          },
+        };
   });
+
+  useEffect(() => {
+    localStorage.setItem("formData", JSON.stringify(formData));
+  }, [formData]);
 
   return (
     <FormContext.Provider value={{ formData, setFormData }}>
